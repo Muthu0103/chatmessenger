bee3e29b7c3caad38713e82f2ee30e72a6a82066
Muthu0103 <arrownvig2001@gmail.com>
2023-11-30 14:19:58 +0530

    Initial commit

---
+++ app/src/main/java/com/example/chatmessenger/notifications/Constants.kt
@@ -0,0 +1,9 @@
+package com.example.chatmessenger.notifications
+
+class Constants {
+    companion object {
+        const val BASE_URL = "https://fcm.googleapis.com"
+        const val SERVER_KEY = "{AAAAP0Oc1g8:APA91bEzYgpM-OQxRn1mtqvVJrgkdab3_W4rViWqdPxzhQsZpjQjf-9Hj2rbvYuhOl4R-hssHimnZERYjKph2WKx-IIdfO8fx5crRYrEgfPtvuXfOpHvb1aFOicAam22RqtHJ2DnF0AJ}"
+        const val CONTENT_TYPE = "application/json"
+    }
+}
